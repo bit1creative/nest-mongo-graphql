c9e54f41a9f88a32bfd283e0abfaa97fda66bae9
bit1creativee <ivan.hrytsenko@nbcuni.com>
2023-02-04 17:04:23 +0200

    [mongo graphql]

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { LessonModule } from './lesson/lesson.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
+import { Student } from './student/student.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson, Student],
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    LessonModule,
+  ],
+})
+export class AppModule {}
---
+++ src/student/student.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { StudentService } from './student.service';
+import { StudentResolver } from './student.resolver';
+import { Student } from './student.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Student])],
+  controllers: [],
+  providers: [StudentService, StudentResolver],
+  exports: [StudentService],
+})
+export class StudentModule {}
